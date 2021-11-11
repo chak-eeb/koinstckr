0a077a48f709d6d0319058f9c3a8038e32540459
chakib <chak_eeb@yahoo.com>
2021-11-11 11:29:47 -0500

    make price color and arrow direction dynamic using ternary operator

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -8,9 +8,17 @@ const Coin = ({coin}) => {
             <img  src={coin.image} alt=''/>
             <div className='d-flex w-50 justify-content-between align-items-center'>
             <span className="text-decoration-none">{coin.current_price}</span>
+            {coin.price_change_percentage_24h > 0 ? 
             <span className="text-success mr-2">
-            <i className="fas fa-sort-up align-middle mr-2"></i>
-                {coin.price_change_percentage_24h}</span>
+                <i className="fas fa-sort-up align-middle mr-2"></i>
+                {coin.price_change_percentage_24h.toFixed(2)}
+            </span>
+                : 
+                <span className="text-danger mr-2">
+                    <i className="fas fa-sort-down align-middle mr-2"></i>
+                    {coin.price_change_percentage_24h.toFixed(2)}
+                </span>}
+                
                 <i className="delete-icon far fa-times-circle text-danger"></i>
             </div>
         </li>
